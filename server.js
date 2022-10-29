8fe17ac88cdaa85c99bdf244afaf8ce1cba7df98
lafupra <cpraful966@gmail.com>
2022-10-29 20:04:58 +0530

    middlewear

--- server.js
+++ server.js
@@ -8,9 +8,27 @@ import commentRoute from "./routes/commentRoute.js"
 
 const app = express()
 
-app.use(cors({  origin:'http://localhost:3000', 
-credentials:true,            //access-control-allow-credentials:true
-optionSuccessStatus:200}))
+app.use(cors())
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.use(express.json())
 
